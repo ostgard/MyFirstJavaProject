bdb913b085668a760b926a88864efe50fe83bff1
es <erika.sundman@bjornlunden.com>
2024-10-28 14:37:16 +0100

    hello master

--- src/Exercise11/Exercise11.java
+++ src/Exercise11/Exercise11.java
@@ -2,6 +2,6 @@ package Exercise11;
 
 public class Exercise11 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello Master");
     }
 }
